017ea750ed0ea7befd9995c36e0ea0ebff326e34
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2016-06-26 16:13:57 -0300

    Adding mysql driver

--- config.py
+++ config.py
@@ -9,8 +9,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Define the database - we are working with
 # SQLite for this example
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'app.db')
-DATABASE_CONNECT_OPTIONS = {}
+SQLALCHEMY_DATABASE_URI = config('SQLALCHEMY_DATABASE_URI')
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle

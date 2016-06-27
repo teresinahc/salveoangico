16f5ea5066cebcf94874eaaec95ca37073273444
Gustavo Carvalho <gt.salles@gmail.com>
2016-06-26 21:57:26 -0300

    add sqlalchemy database url

--- config.py
+++ config.py
@@ -10,6 +10,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
 # SQLite for this example
 DATABASE_URL = config('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle

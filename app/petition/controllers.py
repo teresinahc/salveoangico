b414e8e0f92769adb132ac7695c356f277578a34
Rafael Guimarães <rafaelguimaraesweb@gmail.com>
2016-06-27 13:04:41 -0300

    Add pagination

--- app/petition/controllers.py
+++ app/petition/controllers.py
@@ -3,6 +3,7 @@
 from flask import Blueprint, request, render_template, \
                   flash, g, session, redirect, url_for
 
+from config import RESULTS_PER_PAGE
 from app import db
 from app.petition.forms import SignatureForm
 from app.petition.models import Signature
@@ -10,13 +11,13 @@ from sqlalchemy import func
 
 mod_petition = Blueprint('petitions', __name__)
 
-
 @mod_petition.route('/', methods=['GET', 'POST'])
-def create():
+@mod_petition.route('/<int:page>', methods=['GET', 'POST'])
+def create(page=1):
 
     form = SignatureForm(request.form)
     signatures_count = Signature.query.count()
-    signatures = Signature.query.order_by('date_created DESC').all()
+    signatures = Signature.query.order_by('date_created DESC').paginate(page, RESULTS_PER_PAGE, False)
 
     if form.validate_on_submit():
         signature = Signature(
